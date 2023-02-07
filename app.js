c65a4613e08c233de1023c64d3209deff11efb45
Nathan Pedigo <njpedigo@gmail.com>
2023-02-07 08:02:49 -0700

    did a nice warmup on grapping the username and password from typed text

---
+++ app.js
@@ -0,0 +1,5 @@
+function submitForm() {
+    let nameText = document.getElementById('username').value;
+    let passWordText = document.querySelector('input[type="password"]').value;
+    console.log(nameText, passWordText);
+}
